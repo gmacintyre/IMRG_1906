d54c5524c25392494d796a4d736598c1c9840c71
Gina MacIntyre <gvmacintyre@gmail.com>
2020-01-25 13:08:23 -0500

    initial commit for empty project

---
+++ IMRG_1906/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IMRG_1906.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
