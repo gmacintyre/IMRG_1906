17e887a96e2a7d8a24487bad3b6d24fea7ef6eed
Gina MacIntyre <gvmacintyre@gmail.com>
2020-01-25 14:42:49 -0500

    added dal project

---
+++ imrg_db/Event.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Event]
+(
+	[EventId] INT IDENTITY (1, 1) NOT NULL,
+	[EventName] NVarChar(2000) Not NULL,
+	[Inserted_By] UniqueIdentifier Not Null,
+	[Inserted_Date] DATETIME Not Null
+)
