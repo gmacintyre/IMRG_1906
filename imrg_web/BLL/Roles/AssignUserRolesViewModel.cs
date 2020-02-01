98a039ed60e972f39d0390022dcc405909567ede
Gina MacIntyre <gvmacintyre@gmail.com>
2020-02-01 17:26:36 -0500

    added role creation and assignment

---
+++ imrg_web/BLL/Roles/AssignUserRolesViewModel.cs
@@ -0,0 +1,27 @@
+﻿using imrg_web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace imrg_web.BLL.Roles
+{
+    public class AssignUserRolesViewModel
+    {
+        public AspNetUser User { get; set; }
+        public string UserId { get; set; }
+        public List<AspNetRole> AvailableRoles { get; set; }
+        public string RoleId { get; set; }
+
+        public static AssignUserRolesViewModel Create(AspNetUser user, List<AspNetRole> roles)
+        {
+            var model = new AssignUserRolesViewModel();
+
+            model.User = user;
+            model.UserId = user.Id;
+            model.AvailableRoles = roles;
+
+            return model;
+        }
+    }
+}
---
+++ imrg_web/Controllers/UserRolesController.cs
@@ -0,0 +1,98 @@
+﻿using imrg_web.BLL.Roles;
+using imrg_web.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace imrg_web.Controllers
+{
+    public class UserRolesController : Controller
+    {
+
+        private imrgEntities db = new imrgEntities();
+        // GET: UserRoles
+        public ActionResult Index()
+        {
+            var users = db.AspNetUsers.ToList();
+            var roles = db.AspNetRoles.ToList();
+            return View(users);
+        }
+
+        public ActionResult AssignUserRoles(string id)
+        {
+            var user = db.AspNetUsers.Find(id);
+            if(user == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            db.Entry(user).Collection(x => x.AspNetRoles).Load();
+            var assignedRoles = user.AspNetRoles.Select(x => x.Id);
+            var roles = db.AspNetRoles.Where(x => !assignedRoles.Contains(x.Id)).ToList();
+
+            var assignUserRolesModel = AssignUserRolesViewModel.Create(user, roles);
+
+            return View(assignUserRolesModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AssignUserRoles([Bind(Include= "UserId, RoleId")]AssignUserRolesViewModel model)
+        {
+            var role = db.AspNetRoles.Find(model.RoleId);
+            if(role == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var user = db.AspNetUsers.Find(model.UserId);
+            if(user == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            if(user.AspNetRoles.FirstOrDefault(x=> x.Id == role.Id) == null)
+            {
+                user.AspNetRoles.Add(role);
+                db.Entry(role).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("AssignUserRoles", new { id = user.Id });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RemoveUserRoles([Bind(Include = "UserId, RoleId")]AssignUserRolesViewModel model)
+        {
+            var role = db.AspNetRoles.Find(model.RoleId);
+            if (role == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var user = db.AspNetUsers.Find(model.UserId);
+            if (user == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            if (user.AspNetRoles.FirstOrDefault(x => x.Id == role.Id) != null)
+            {
+                user.AspNetRoles.Remove(role);
+                db.Entry(role).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("AssignUserRoles", new { id = user.Id });
+        }
+
+
+
+    }
+}
